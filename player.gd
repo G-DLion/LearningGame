fa28f49b125bdb88b08ccc82b936f3f5a1d4aa4c
Linker <89202168+G-DLion@users.noreply.github.com>
2023-04-20 00:17:53 +0300

    поменял управление и коллизию персонажа

--- player.gd
+++ player.gd
@@ -1,15 +1,18 @@
 extends CharacterBody2D
 
-var speed = 150
+var speed = 2
 var jampForce = 500
 var gravity = 700
 
-var vel = Vector2()
+var vel = Vector2(0,0)
 
 func _physics_process(delta):
-	if Input.is_action_pressed("player_left"):
+	if Input.is_action_pressed("Player_Left"):
 		vel.x -= speed
-	elif Input.is_action_pressed("player_right"):
+	elif Input.is_action_pressed("Player_Rihgt"):
 		vel.x += speed
+	vel = vel.normalized() * speed
+	position.x += vel.x
+	vel = Vector2(0,0)
 	
-	move_and_slide(vel, Vector2.UP)
+	
