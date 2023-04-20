64bfbc48446e99e1f47db622bea4556c18b92975
Linker <89202168+G-DLion@users.noreply.github.com>
2023-04-20 19:08:16 +0300

    исправил упраавление

--- player.gd
+++ player.gd
@@ -1,18 +1,32 @@
 extends CharacterBody2D
 
-var speed = 2
+var speed = 400
 var jampForce = 500
 var gravity = 700
 
-var vel = Vector2(0,0)
+var input = Vector2.ZERO
 
 func _physics_process(delta):
-	if Input.is_action_pressed("Player_Left"):
-		vel.x -= speed
-	elif Input.is_action_pressed("Player_Rihgt"):
-		vel.x += speed
-	vel = vel.normalized() * speed
-	position.x += vel.x
-	vel = Vector2(0,0)
+	move_player(delta)
+
 	
+func get_input():
+	input.x = int(Input.is_action_pressed("Player_Rihgt")) - int(Input.is_action_pressed("Player_Left"))
+	return input.normalized()
+
+func move_player(delta):
+	input = get_input()
+	
+	print(velocity.length())
+	
+	if input == Vector2.ZERO:
+		if  velocity.length() * delta > (20 * delta):
+			velocity -= velocity.normalized() * delta * speed
+		else:
+			velocity = Vector2.ZERO
+	else:
+		velocity += input * delta * speed
+		velocity = velocity.limit_length(speed)
+	
+	move_and_slide()
 	
